a93f066359829261ec13b05b46bd57931489d887
Gaurav-Narayan-Varma <gaurav.varma@hey.com>
2025-05-16 21:43:26 -0700

    feat: react query global error handling for  unauth throws

--- apps/web/app/_trpc/provider.tsx
+++ apps/web/app/_trpc/provider.tsx
@@ -1,12 +1,44 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-
+import toast from "react-hot-toast";
 import { trpc } from "./client";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient({}));
+  const router = useRouter();
+
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: (error: any) => {
+        if (error?.data?.code === "UNAUTHORIZED") {
+          queryClient.clear();
+          router.push("/auth/chef-login");
+          toast.error(
+            "You are not authorized to access this page. Please login."
+          );
+        }
+      },
+    }),
+    mutationCache: new MutationCache({
+      onError: (error: any) => {
+        if (error?.data?.code === "UNAUTHORIZED") {
+          queryClient.clear();
+          router.push("/auth/chef-login");
+          toast.error(
+            "You are not authorized to access this page. Please login."
+          );
+        }
+      },
+    }),
+  });
+
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
