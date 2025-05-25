86d0b8abca07b957d7017cb429c81d01b8116078
Gaurav-Narayan-Varma <gaurav.varma@hey.com>
2025-05-24 21:06:17 -0700

    Revert "experimental: api cors issue"
    This reverts commit d2cd8dd2afb9c643edc2961d30405b70d9ae8582.

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -1,29 +1,13 @@
 import { CorsOptions } from "cors";
 
 export const corsOptions: CorsOptions = {
+  // We need to pass `true` to let the browser know to store the cookie
+  // we include in the `Set-Cookie` header.
   credentials: true,
-  // Add more detailed origin handling
-  origin: (origin, callback) => {
-    console.log("Incoming request origin:", origin);
-    
-    const allowedOrigins = [
-      'http://localhost:3001',
-      'https://feast-hnbz.onrender.com',
-      'https://feast-hnbz.onrender.com/' // with trailing slash
-    ];
-    
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) {
-      return callback(null, true);
-    }
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      console.log("Origin not allowed:", origin);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  // We need to pass the origin of the web app explicitly (rather than `*`)
+  // otherwise we get a CORS error (due to credentials: true).
+  origin: process.env.FEAST_WEB_URL,
+  // We need to be explicit about what headers are allowed.
   allowedHeaders: ["Content-Type", "Cookie"],
 };
 
