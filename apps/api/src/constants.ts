6ff01fa1625244474fd38a8d624446d157b936f0
Gaurav-Narayan-Varma <gaurav.varma@hey.com>
2025-05-24 21:43:47 -0700

    Revert "cors experimental"
    This reverts commit d598c94a5bac3012ad8745c907121028d330696c.

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -8,20 +8,7 @@ export const corsOptions: CorsOptions = {
   // otherwise we get a CORS error (due to credentials: true).
   origin: process.env.FEAST_WEB_URL,
   // We need to be explicit about what headers are allowed.
-  allowedHeaders: [
-    "Content-Type",
-    "Cookie",
-    "Authorization",
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Origin",
-    "Access-Control-Allow-Methods",
-  ],
-  // Specify which headers the browser is allowed to expose to the client
-  exposedHeaders: [
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Origin",
-    "Access-Control-Allow-Methods",
-  ],
+  allowedHeaders: ["Content-Type", "Cookie"],
 };
 
 export const MAX_COOKIE_AGE = 60 * 60 * 24 * 365;
@@ -33,16 +20,16 @@ export const MAX_COOKIE_AGE = 60 * 60 * 24 * 365;
 
 export const ALLOWED_ORIGINS = [
   // Production URLs
-  "https://joinfeastco.com",
-  "https://www.joinfeastco.com",
+  'https://joinfeastco.com',
+  'https://www.joinfeastco.com',
   // Local development URLs
-  "http://localhost:8080",
-  "http://www.localhost:8080",
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "http://localhost:8000",
-  "http://localhost:4000",
-  "http://localhost:4200",
-  "http://localhost:5000",
-  "http://localhost:3001",
+  'http://localhost:8080',
+  'http://www.localhost:8080',
+  'http://localhost:3000',
+  'http://localhost:5173', 
+  'http://localhost:8000',
+  'http://localhost:4000',
+  'http://localhost:4200',
+  'http://localhost:5000',
+  'http://localhost:3001',
 ];
